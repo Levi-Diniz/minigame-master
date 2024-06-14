052f3b9aa1abe5c41097b6f91decd0eda0fcaa0a
PzGlevi <pzglevitpb46@gmail.com>
2024-06-14 17:44:20 -0300

    "Uncommented showModal() call in Jogo function"

--- src/components/Teclas/Teclas.js
+++ src/components/Teclas/Teclas.js
@@ -80,7 +80,7 @@ export default function Jogo() {
                 setTimeUp(true);
                 setGameOver(true);
                 clearTimeout(timer);
-                // showModal();
+                showModal();
             }
         }, intervalTime);
 
